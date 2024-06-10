98f1a84cad54b504943fc616ee44da989babc61a
praveen14m10 <89249946+praveen14m10@users.noreply.github.com>
2024-06-10 16:39:28 +0530

    Update settings.py

--- testapi/testapi/settings.py
+++ testapi/testapi/settings.py
@@ -91,14 +91,25 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'restapi',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Iness*123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restapi',
-        'USER': 'postgres',
-        'PASSWORD': 'Iness*123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': 'defaultdb',
+        'USER': 'avnadmin',
+        'PASSWORD': 'AVNS_5S_rMK0OJC2_BSyHzc8',
+        'HOST': 'pg-2618a7ea-rest-api-5eec.k.aivencloud.com',
+        'PORT': '21724',
     }
 }
 
